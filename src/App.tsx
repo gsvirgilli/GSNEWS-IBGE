040de29750fdd21bb8d3c50027247b02b57accdd
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-09-20 03:29:08 -0300

    estou um pouco confuso como farei

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 
 function App() {
   return (
-    <Header />
+    <Routes>
+      <Route path="/" element={ <Header /> } />
+    </Routes>
   )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,33 @@
+import { useIBGE } from '../../contexts/ibgeContext';
 import style from './Header.module.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const { apiData } = useIBGE();
+
+  if (apiData.length === 0) {
+    return null;
+  }
+
   return (
     <div className={ style.header }>
       <img className={ style.imag } src="" alt="logo gs news" />
       <h1 className={ style.gsnews }>GS NEWS</h1>
       <div>
-        <Link to={''} />
-        
+        <Link target='_blank' to={'https://github.com/gsvirgilli'}>
+          <img src="src/imgs/github.svg" alt="GitHub" />
+        </Link>
+        <Link target='_blank' to={'https://www.linkedin.com/in/gsvirgilli/'}>
+          <img src="src/imgs/linkedin.svg" alt="LinkedIn" />
+        </Link>
+      </div>
+      <div key={apiData[0].id}>
+        <img src="" alt="imagens" />
+        <h3>{apiData[0].titulo}</h3>
+        <p>{apiData[0].introducao}</p>
+        <p>{apiData[0].data_publicacao}</p>
+        <Link target='_black' to={apiData[0].link}>Noticia Completa</Link>
+        <button><img src="" alt="Favoritos" /></button>
       </div>
     </div>
   );
