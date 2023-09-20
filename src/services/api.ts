3986394b6b8d38008895311ba82dade3fff27b80
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-09-20 01:08:48 -0300

    Verificando se a Api esta retornando

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ const URL_IBGE = 'https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=100';
 export const getApi = async () => {
   const response = await fetch(URL_IBGE);
   const data = await response.json();
-  return data;
+  return data.items;
 }
