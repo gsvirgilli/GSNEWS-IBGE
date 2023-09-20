7d690023042f6306c1226bd0b2fc4a3388ab124d
Guilherme Virgilli <gsvirgilli@gmail.com>
2023-09-20 01:32:16 -0300

    Verificando se a Api esta retornando

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useIBGE } from './contexts/ibgeContext'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 
 function App() {
-  const { apiData } = useIBGE();
   return (
-    <>
     <Header />
-    {apiData.map((ibge) => (
-      <div key={ibge.id}>
-        <p>{ibge.link}</p>
-      </div>
-    ))}
-    </>
   )
 }
 
